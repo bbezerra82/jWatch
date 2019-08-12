46ab570a2f65315644e9357a96cfe708f9e3413f
Bernardo <b.alvim@gmail.com>
2019-08-12 06:10:39 +0100

    added tmdb logic plus new strings

--- lambda/custom/i18n/en.js
+++ lambda/custom/i18n/en.js
@@ -4,13 +4,15 @@ module.exports = {
     translation: {
         'SKILL_NAME' : 'Where to Watch',
         'ADJECTIVES' : [
-            'crazy',
-            'awesome',
-            'amazing',
-            'exciting'
+            'a crazy',
+            'an awesome',
+            'an amazing',
+            'an exciting',
+            'an epic',
+            'a mind blowing'
         ],
         'WELCOME_MESSAGE' : [
-            'Welcome to %s, your %s skill to check where to stream movies and TV shows. Just ask me to search for a movie or TV show and I will let you know in which service you can stream, rent or buy it.',
+            'Welcome to %s, %s skill to check where to stream movies and TV shows. Just ask me to search for a movie or TV show and I will let you know in which service you can stream, rent or buy it.',
         ],
         'REPROMPT_MESSAGE' : [
             'What movie or TV show do you want to watch?'
@@ -19,6 +21,70 @@ module.exports = {
             'Talk to you later',
             'See you soon',
             'Bye bye'
+        ],
+        'RESULT_MESSAGE' : [
+            'Here is the <say-as interpret-as="ordinal">%s</say-as> result: %s from %s with %s: %s',
+            'The <say-as interpret-as="ordinal">%s</say-as> result is: %s from %s with %s: %s',
+            'Here is the <say-as interpret-as="ordinal">%s</say-as> result: %s from %s with %s: %s',
+            'The <say-as interpret-as="ordinal">%s</say-as> result from the search is: %s from %s with %s: %s'
+        ],
+        'CONFIRMATION_MESSAGE' : [
+            ' Is this the one you are looking for?',
+            ' Is this the expected result?',
+            ' Is this ok?',
+            ' Is this the right one?'
+        ],
+        'END_OF_RESULTS' : [
+            'These were all the results I could find for %s. ',
+            'There are no more results for %s. ',
+            'You reached the end of the list for %s. '
+        ],
+        'END_OF_RESULTS_FOLLOW_UP' : [
+            'Do you want to search for a different one?',
+            'Do you want to search for for a different title?',
+            'Is there a different title you want me to sarch for?'
+        ],
+        'YOU_CAN_STREAM' : [
+            'You can stream %s on ',
+            '%s is available for streaming on ',
+            'Stream %s on '
+        ],
+        'NO_STREAM' : [
+            'I couldn\'t find %s in any streaming service. ',
+            'There were no results for %s in the streaming services. ',
+            '%s is not available in any streaming service. '
+        ],
+        'BUT_RENT_BUY' : [
+            'But I found some offers for buying or renting. ',
+            'There are results for buying or renting though. ',
+            'However, I did find it in some providers if you are interested in renting or buying it. '
+        ],
+        'RENT_BUY_FOLLOW_UP' : [
+            'Do you want to hear them?',
+            'Are you interested in hearing them?',
+            'Do you want me to tell you what they are?'
+        ],
+        'BUT_RENT' : [
+            'But I found some offers for renting. ',
+            'There are results for renting though. ',
+            'There are also results for renting though. ',
+            'However, I did find it in some providers if you are interested in renting. '
+        ],
+        'BUT_BUY' : [
+            'But I found some offers for buying. ',
+            'There are results for buying though. ',
+            'There are also results for buying though. ',
+            'However, I did find it in some providers if you are interested in buying. '
+        ],
+        'YOU_CAN_RENT' : [
+            'You can rent %s from ',
+            '%s is available for rent on ',
+            'Rent %s from '
+        ],
+        'YOU_CAN_BUY' : [
+            'You can buy %s on ',
+            '%s is available for purchase on ',
+            'Buy %s from '
         ]
     }
 };
--- lambda/custom/i18n/pt.js
+++ lambda/custom/i18n/pt.js
@@ -2,9 +2,34 @@
 module.exports = {
     translation: {
         'SKILL_NAME' : 'Aonde Assistir',
+        'ADJECTIVES' : [
+            'incrível',
+            'fantástica',
+            'impressionante',
+            'excitante'
+        ],
         'WELCOME_MESSAGE' : [
-            'Bem vindo',
-            ''
+            'Seja bem vindo a %s, sua skill %s para verificar aonde fazer streaming de filmes e séries. Simplesmente me peça para procurar por um filme ou série e eu vou te dizer em qual serviço você pode fazer o streaming, alugar ou comprar.',
+        ],
+        'REPROMPT_MESSAGE' : [
+            'Qual filme ou série você quer assistir?'
+        ],
+        'FAREWELL_MESSAGE' : [
+            'Nos falamos mais tarde',
+            'Até mais',
+            'Tchau'
+        ],
+        'TOP_RESULT_MESSAGE' : [
+            'Aqui está o primeiro resultado: %s de %s: %s',
+            'O primeiro resultado é: %s de %s: %s',
+            'O primeiro resultado da minha busca é: %s de %s: %s'
+        ],
+        'CONFIRMATION_MESSAGE' : [
+            ' É o que você estava procurando?',
+            ' É o resultado que você espserava?',
+            ' Esse resultado tá ok?',
+            ' Você quer ouvir outros resultados?',
+            ' É este o resultado correto?'
         ]
     }
 }
